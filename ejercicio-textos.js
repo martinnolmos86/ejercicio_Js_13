757a21f4dc04b2414910a446e646728544e2c448
Martin Olmos <martinnolmos@gmail.com>
2023-07-19 09:31:38 -0300

    first commit

---
+++ ejercicio-textos.js
@@ -0,0 +1,7 @@
+const textoConComillasSimples = "Texto con comillas simples";
+const textoConComillasDobles = "Texto con comillas dobles";
+const textoConBackticks = `Texto con backticks`;
+
+console.log(textoConComillasSimples);
+console.log(textoConComillasDobles);
+console.log(textoConBackticks);
